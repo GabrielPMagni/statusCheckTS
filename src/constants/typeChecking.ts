4d33a60463a61fce7245fb915e8b806bf22abe09
Gabriel Peres Magni <gpmagni@protonmail.com>
2023-01-11 02:14:53 -0300

    fix url regular expression

--- src/constants/typeChecking.ts
+++ src/constants/typeChecking.ts
@@ -1,3 +1,3 @@
 export const isValidURL = (textToValidate: string) => {
-    return new RegExp('(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?').test(textToValidate);
+    return new RegExp(/[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/).test(textToValidate);
 }
